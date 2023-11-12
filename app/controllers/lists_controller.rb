98471685e5e17f862bd8a27930a928ff01e51056
YuIizuka <148466726+iizuka1895@users.noreply.github.com>
2023-11-12 13:26:09 +0000

    add 11/12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,32 +1,50 @@
 class ListsController < ApplicationController
-  
+
   def new
     # viewへ渡すためのインスタンス変数にからのオブジェクトを生成する
     @list = List.new
   end
-  
+
   def create
-    # 1.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    @list = List.new(list_params)
+  
+    if @list.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(@list.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :new
+    end
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-  
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
   private
   #　ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
-  
+
 end
